2c1ad978a6f34d412b0e4d370915a0b6ac4c13a1
Sushanti99 <sushantii.kerani@gmail.com>
2018-03-29 10:54:26 +0530

    Update feature_detection.py

--- feature_detection.py
+++ feature_detection.py
@@ -17,6 +17,7 @@ dst = cv2.dilate(dst,None)
 # Threshold for an optimal value, it may vary depending on the image.
 img[dst>0.01*dst.max()]=[0,0,255]
 
+print(dst)
 
 cv2.imshow('stones',img)
 if cv2.waitKey(0) & 0xff == 27:
